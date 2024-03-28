e71b5d45f0d7776791e5f9174eedbb4d7f39e4a9
JY2345 <ilokids@gmail.com>
2024-03-28 20:52:30 +0900

    goods

--- src/goods/dto/update-goods.dto.ts
+++ src/goods/dto/update-goods.dto.ts
@@ -1,4 +1,4 @@
 import { PickType } from "@nestjs/swagger";
 import { Goods } from "../entities/goods.entity";
 
-export class UpdateGoodDto extends PickType(Goods, ['id']) {}
+export class UpdateGoodDto extends PickType(Goods, ["id"]) {}
