1a8ee9b9a8296cd28ef58f86a55f5fd569a1c424
GGANGTONG <qkrds0914@gmail.com>
2024-04-03 16:46:30 +0900

    테스트코드 ordersService 제외 작성 완료

---
+++ src/orders/orders.service.spec.ts
@@ -0,0 +1,123 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { OrdersService } from './orders.service';
+import { ArgumentMetadata, ValidationPipe } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { Orders } from './entities/orders.entity';
+import { DataSource, Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+describe('PaymentsService', () => {
+  let service: OrdersService;
+  let ordersRepository: Partial<Record<keyof Repository<Orders>, jest.Mock>>;
+  
+  type MockType<T> = {
+    [P in keyof T]?: jest.Mock<T>;
+  };
+  
+  const dataSource: Partial<MockType<DataSource>> = {
+    createQueryRunner: jest.fn().mockImplementation(() => ({
+      connect: jest.fn(),
+      startTransaction: jest.fn(),
+      release: jest.fn(),
+      commitTransaction: jest.fn(),
+      rollbackTransaction: jest.fn(),
+      manager: {
+        update: jest.fn().mockImplementation(),
+        save: jest.fn().mockReturnThis(), 
+        create: jest.fn().mockReturnThis(),
+        findOne: jest.fn().mockImplementation()
+     }
+    }))
+  }
+
+  async function validation(Dto, dto) {
+    const validatonPipe = new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    });
+    const metadata: ArgumentMetadata = {
+      type: 'body',
+      metatype: Dto,
+      data: '',
+    };
+    await validatonPipe.transform(dto, metadata)
+  }
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        OrdersService, 
+        {
+          provide: DataSource,
+          useValue: dataSource
+        },
+        {
+          provide: getRepositoryToken(Orders),
+          useValue: ordersRepository
+        }
+        ],
+    }).compile();
+
+    ordersRepository = {
+      create: jest.fn(),
+      save: jest.fn()
+    }
+
+    
+
+    service = module.get<OrdersService>(OrdersService);
+  })
+  
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('1-1. Orders - purchase, userId와 createOrderDto를 정상적으로 전송받아 결제 완료를 반환함', async () => {
+    const mockQueryRunner = await dataSource.createQueryRunner()
+
+    const userId = 1
+    const createOrderDto:CreateOrderDto = {
+      o_tel: '010-0000-0000',
+      o_addr: '국밥광역시 국밥구 국밥동 국밥아파트',
+      o_count: 15,
+      o_req: '섭씨 150도짜리 국밥',
+      goods_id: 1
+    }
+    await validation(CreateOrderDto, createOrderDto)
+
+    console.log('국밥', mockQueryRunner)
+    dataSource.createQueryRunner().connect()
+
+    mockQueryRunner.manager.findOne.mockResolvedValueOnce('구매하려는 상품').mockResolvedValueOnce('구매를 원하는 유저')
+    
+    mockQueryRunner.manager.update.mockResolvedValueOnce('재고 갱신')
+    mockQueryRunner.manager.save.mockResolvedValueOnce('유저 포인트 갱신')
+
+  })
+
+
+});
+
+
+// const dataSource: () => MockType<DataSource> = jest.fn(() => ({
+//   createQueryRunner: jest.fn().mockImplementation(() => ({
+//     connect: jest.fn(),
+//     startTransaction: jest.fn(),
+//     release: jest.fn(),
+//     commitTransaction: jest.fn(),
+//     rollbackTransaction: jest.fn(),
+//     manager: {
+//       update: jest.fn(),
+//       save: jest.fn(), 
+//       create: jest.fn(),
+//       findOne: jest.fn()
+//    }
+//   }))
+// }))
--- src/user/users.service.spec.ts
+++ src/user/users.service.spec.ts
@@ -5,11 +5,12 @@ import { Users } from './entities/user.entitiy';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { SignUpDto } from './dto/signup.dto';
-import { ArgumentMetadata, ValidationPipe } from '@nestjs/common';
+import { ArgumentMetadata, BadRequestException, NotFoundException, ValidationPipe } from '@nestjs/common';
 import { SignInDto } from './dto/sign_in.dto';
 import { hash } from 'bcrypt';
+import { UpdateDto } from './dto/update.dto';
 
-describe('PaymentsService', () => {
+describe('UsersService', () => {
   let service: UserService;
   let usersRepository: Partial<Record<keyof Repository<Users>, jest.Mock>>;
   let jwtService: Partial<JwtService>;
@@ -25,9 +26,7 @@ describe('PaymentsService', () => {
       metatype: Dto,
       data: '',
     };
-    await validatonPipe.transform(dto, metadata).catch((err) => {
-      return expect(err).toEqual(err);
-    });
+    await validatonPipe.transform(dto, metadata)
   }
 
   beforeEach(async () => {
@@ -77,8 +76,10 @@ describe('PaymentsService', () => {
       nickname: '국밥대장',
       profile: '국밥사진',
     };
+    await validation(SignUpDto, signUpDto)
+
     const mockedReturn = '회원가입 테스트 성공!';
-    await usersRepository.save.mockReturnValueOnce(mockedReturn);
+    usersRepository.save.mockReturnValueOnce(mockedReturn);
 
     const result = await service.register(signUpDto).catch((err) => {
       throw new Error(err);
@@ -95,7 +96,9 @@ describe('PaymentsService', () => {
       profile: '국밥사진',
     };
 
-    await validation(SignUpDto, signUpDto);
+    await validation(SignUpDto, signUpDto).catch((err) => {
+      return expect(err).toEqual(err);
+    });;
   });
 
   it('1-3. User - register, 같은 이메일이 이미 가입되어 있어 에러가 발생함', async () => {
@@ -107,7 +110,7 @@ describe('PaymentsService', () => {
       profile: '국밥사진',
     };
     const sameEmail = 'gookbab99@gmail.com';
-    await usersRepository.findOne.mockResolvedValueOnce(sameEmail);
+    usersRepository.findOne.mockResolvedValueOnce(sameEmail);
 
     // const mockedReturn = '회원가입 테스트 성공!'
     // await usersRepository.save.mockReturnValueOnce(mockedReturn)
@@ -128,7 +131,7 @@ describe('PaymentsService', () => {
     await validation(SignInDto, signInDto);
 
     const hashedPassword = await hash(signInDto.password, 10);
-    await usersRepository.findOne.mockResolvedValueOnce({
+    usersRepository.findOne.mockResolvedValueOnce({
       id: 1,
       email: 'gookbab99@gmail.com',
       password: `${hashedPassword}`,
@@ -154,7 +157,9 @@ describe('PaymentsService', () => {
       password: 'Ex@mple123!!!',
     };
 
-    await validation(SignInDto, signInDto);
+    await validation(SignInDto, signInDto).catch((err) => {
+      return expect(err).toEqual(err);
+    });;
   });
 
   it('2-3. User - signIn, 가입된 이메일이 존재하지 않아 에러를 반환함', async () => {
@@ -167,14 +172,188 @@ describe('PaymentsService', () => {
 
     await validation(SignInDto, signInDto);
 
-    await usersRepository.findOne.mockResolvedValueOnce(undefined || null);
-    const result = await service.signIn(signInDto).catch((err) => {
-      expect(err).toThrowError('이메일을 확인하세요.');
+    usersRepository.findOne.mockResolvedValueOnce(undefined || null);
+
+    await expect(service.signIn(signInDto)).rejects.toThrow('이메일을 확인하세요.')
+  });
+
+  it('2-4. User - signIn, 입력한 비밀번호와 db에 저장된 비밀번호가 일치하지 않아 에러를 반환함', async () => {
+    //refreshToken과 accessToken 중 하나만 발급되고 중단되는 경우가 있을 수 있을까?
+    //
+    const signInDto: SignInDto = {
+      email: 'gookbab99@gmail.com',
+      password: 'Ex@mple123!!!',
+    };
+
+    await validation(SignInDto, signInDto);
+
+    const hashedPassword = await hash('Ex@mple456???', 10);
+    usersRepository.findOne.mockResolvedValueOnce({
+      id: 1,
+      email: 'gookbab99@gmail.com',
+      password: `${hashedPassword}`,
+      nickname: '국밥대장',
+      profile: '국밥사진',
+      name: '국밥',
     });
 
-    expect(result).toStrictEqual({
-      accessToken: 'jwt-token',
-      refreshToken: 'jwt-token',
-    });
+    await expect(service.signIn(signInDto)).rejects.toThrow('비밀번호를 확인하세요.')
   });
+
+  it('3-1. User - findAll, 모든 유저를 id 오름차순으로 반환함', async () => {
+
+    const users = [
+      {
+        id: 1,
+        email: 'gookbab99@gmail.com',
+        nickname: '국밥대장',
+        profile: '국밥사진',
+        name: '국밥',
+      },
+      {
+        id: 2,
+        email: 'gookbab99@gmail.com',
+        nickname: '감자대장',
+        profile: '감자사진',
+        name: '감자',
+      },
+      {
+        id: 3,
+        email: 'gookbab99@gmail.com',
+        nickname: '고구마대장',
+        profile: '고구마사진',
+        name: '고구마',
+      }
+    ]
+    usersRepository.find.mockResolvedValueOnce(users)
+
+  expect(await service.findAll()).toEqual(users)
+  })
+
+  it('4-2. User - findOne, 특정 id값을 가진 유저를 검색함', async () => {
+    const id = 1
+    const user = 
+      {
+        id: 1,
+        email: 'gookbab99@gmail.com',
+        nickname: '국밥대장',
+        profile: '국밥사진',
+        name: '국밥',
+      }
+    
+    usersRepository.findOneBy.mockResolvedValueOnce(user)
+
+   expect(await service.findOne(id)).toEqual(user)
+  })
+
+  it('4-2. User - findOne, id에 null/undefined가 할당된 채 전달되어 에러를 반환함', async () => {
+    const id = 1
+    const user = null || undefined
+    usersRepository.findOneBy.mockResolvedValueOnce(user)
+
+   expect(service.findOne(id)).rejects.toThrow(new NotFoundException('해당 유저가 없습니다'))
 });
+
+it('5-1. User - update, id와 updateDto가 잘 전달되어 회원 정보 수정에 성공함, update 값 전후를 ', async () => {
+  const id = 1
+
+  const updateDto: UpdateDto = {
+    nickname: '국밥대장',
+    profile: '국밥사진',
+    password: 'Ex@mple123!!!',
+
+  };
+  await validation(UpdateDto, updateDto)
+  usersRepository.findOne.mockReturnValueOnce(updateDto);
+  usersRepository.save.mockReturnValueOnce(updateDto)
+
+  expect(await service.update(id, updateDto)).toBe(updateDto);
+});
+
+it('5-2. User - update, id에 null/undefined가 할당된 채 전송되어 에러를 반환함', async () => {
+  const id = undefined || null
+
+  const updateDto: UpdateDto = {
+    nickname: '국밥대장',
+    profile: '국밥사진',
+    password: 'Ex@mple123!!!',
+
+  };
+  await validation(UpdateDto, updateDto)
+  usersRepository.findOne.mockReturnValueOnce(undefined || null);
+
+  expect(service.update(id, updateDto)).rejects.toThrow(NotFoundException);
+});
+
+it('5-3. User - update, nickname과 profile 둘다 작성하지 않아 변경 사항이 없어서 에러 반환(Patch 요청이라 globalValidationPipe를 탈지 않탈지 모르겠음, 그래서 validationPipe로직은 제외함 - 이건 put에 맵핑돼야 되는 dto임)' ,
+async () => {
+  const id = 1
+
+  const updateDto: UpdateDto = {
+    nickname: '',
+    profile: '',
+    password: 'Ex@mple123!!!',
+
+  };
+
+  usersRepository.findOne.mockReturnValueOnce({
+    name: '국밥',
+      email: 'gookbab99@gmail.com',
+      password: '',
+      nickname: '국밥대장',
+      profile: '국밥사진',
+  });
+
+  expect(service.update(id, updateDto)).rejects.toThrow(BadRequestException);
+});
+
+it('6-1. User - delete, 특정 id값을 가진 유저의 유저정보를 삭제함', async () => {
+  const id = 1
+  const user = 
+    {
+      id: 1,
+      email: 'gookbab99@gmail.com',
+      nickname: '국밥대장',
+      profile: '국밥사진',
+      name: '국밥',
+    }
+
+  usersRepository.findOneBy.mockResolvedValueOnce(user)
+  usersRepository.remove.mockResolvedValueOnce(user)
+
+ expect(service.findOne(id)).resolves.toBe(user)
+})
+
+it('6-2. User - delete, id값에 undefined/null이 할당된 채 전송되어 에러를 반환함', async () => {
+  const id = undefined || null
+  const user = 
+    {
+      id: 1,
+      email: 'gookbab99@gmail.com',
+      nickname: '국밥대장',
+      profile: '국밥사진',
+      name: '국밥',
+    }
+
+  usersRepository.findOneBy.mockResolvedValueOnce(undefined || null)
+  usersRepository.remove.mockResolvedValueOnce(user)
+
+ expect(service.findOne(id)).rejects.toThrow(NotFoundException)
+})
+
+it('7-1. User - findByEmail, 입력된 email에 해당하는 유저 정보를 반환함', async () => {
+  const email = 'gookbab99@gmail.com'
+
+  const user = {
+    id: 1,
+    email: 'gookbab99@gmail.com',
+    nickname: '국밥대장',
+    profile: '국밥사진',
+    name: '국밥'
+}
+
+  usersRepository.findOne.mockResolvedValueOnce(user)
+
+expect(service.findByEmail(email)).resolves.toBe(user)
+
+})})
