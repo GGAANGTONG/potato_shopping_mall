16b58f6a5bdd1ccf34ef4fa3de75ad64882e4d01
세계 <nw0417@gmail.com>
2024-04-03 21:28:00 +0900

    페이먼트 엔티티 생성 및 주문조회 로직 구현

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -6,14 +6,17 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { Goods } from '../goods/entities/goods.entity';
 import { Stocks } from '../goods/entities/stocks.entity';
 import { Users } from '../user/entities/user.entitiy';
+import { Payments } from '../payments/entities/payments.entity';
 
 @Injectable()
 export class OrdersService {
   constructor(
     @InjectRepository(Orders)
     private ordersRepository: Repository<Orders>,
+    @InjectRepository(Payments)
+    private paymentsRepository: Repository<Payments>,
     private readonly dataSource: DataSource,
-  ) {}
+  ) { }
 
   async purchase(
     userId: number,
@@ -49,17 +52,7 @@ export class OrdersService {
       if (!user) {
         throw new BadRequestException('존재하지 않는 유저입니다.');
       }
-
       const paying = goods.g_price * o_count;
-      const afterPaidPoints = user.points - paying;
-      if (afterPaidPoints < 0) {
-        throw new BadRequestException('포인트가 부족합니다.');
-      }
-
-      user.points = afterPaidPoints;
-      await queryRunner.manager.update(Stocks, { goods }, { count });
-      await queryRunner.manager.save(Users, user);
-
       const newOrder = this.ordersRepository.create({
         user_id: userId,
         o_name: user.name,
@@ -70,8 +63,30 @@ export class OrdersService {
         o_total_price: paying,
         //goods_id 삭제함
       });
-
       await this.ordersRepository.save(newOrder);
+
+      const totalpay = goods.g_price * o_count;
+      const afterPaidPoints = user.points - totalpay;
+      if (afterPaidPoints < 0) {
+        throw new BadRequestException('포인트가 부족합니다.');
+      }
+
+      user.points = afterPaidPoints;
+      await queryRunner.manager.update(Stocks, { goods }, { count });
+      await queryRunner.manager.save(Users, user);
+
+      const newPayments = this.paymentsRepository.create({
+        user_id: userId,
+        p_name: user.name,
+        p_tel: o_tel,
+        p_addr: o_addr,
+        p_count: o_count,
+        p_total_price: totalpay,
+        paid: true
+      });
+
+
+      await this.paymentsRepository.save(newPayments);
       await queryRunner.commitTransaction();
       await queryRunner.release();
 
@@ -83,4 +98,20 @@ export class OrdersService {
       throw err;
     }
   }
+
+  // 유저별 주문 목록 전체 조회
+  async findAllOrderbyUser(userId: number): Promise<Orders[]> {
+    return this.ordersRepository.find({ where: { user_id: userId } });
+  }
+
+  // 전체 주문 정보 확인
+  async findAllOrderbyAdmin(): Promise<Orders[]> {
+    return this.ordersRepository.find();
+  }
+
+  // 상세 주문 정보 확인
+  async findOneOrderbyBoth(orderId: number): Promise<Orders> {
+    return this.ordersRepository.findOne({ where: { id: orderId } });
+  }
+
 }
