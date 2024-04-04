2eea349263d2ce3872d52498a9ad2829bbd3070b
GGANGTONG <qkrds0914@gmail.com>
2024-04-04 21:36:12 +0900

    board 모듈 수정

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
+import { Boards } from './entities/boards.entity';
+import { Users } from 'src/user/entities/user.entitiy';
+import { Comments } from './entities/comments.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Boards, Comments, Users])
+  ],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
