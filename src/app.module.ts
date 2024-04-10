48208db1fa85de8ddc51090ab12e4aba76512912
JY2345 <ilokids@gmail.com>
2024-04-10 11:50:17 +0900

    new file:   src/storage/storage.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { PointModule } from './point/point.module';
 import { RedisModule } from './redis/redis.module';
 import { BoardsModule } from './boards/boards.module';
 import { CacheModule } from '@nestjs/cache-manager';
+import { StorageModule } from './storage/storage.module';
 
 @Module({
   imports: [
@@ -33,6 +34,7 @@ import { CacheModule } from '@nestjs/cache-manager';
     PointModule,
     RedisModule,
     BoardsModule,
+    StorageModule,
   ],
   controllers: [AppController],
   providers: [],
