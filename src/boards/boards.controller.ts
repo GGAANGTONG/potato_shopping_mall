36147e279d39315bd3292d6d166c2cfb8c5ad2e8
GGANGTONG <qkrds0914@gmail.com>
2024-04-10 13:35:12 +0900

    log4js 6계층 로그 기능 구현 완료 및 boards 모듈 로그 적용 완료

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,21 +1,24 @@
-import { Body, Controller, Post, UseInterceptors, Request, UploadedFile, Get, Param, Patch, Delete } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors, Request, UploadedFile, Get, Param, Patch, Delete, UseGuards } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ResizeImagePipe } from '../common/pipe/resize-image.pipe';
 import { UpdateBoardDto } from './dto/update-board.dto';
-import logger from 'src/common/log/logger';
+import logger from '../common/log/logger';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private readonly boardsService: BoardsService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   @UseInterceptors(FileInterceptor('file'))
   create(@UploadedFile(new ResizeImagePipe(400, 400)) file: Express.Multer.File, @Request() req, @Body() createBoardDto:CreateBoardDto) {
     
     const userId = req.user.id 
     logger.traceLogger(`입력 정보 = file: ${file} & req.user: ${JSON.stringify(req.user)} & createBoardDto: ${JSON.stringify(createBoardDto)}`, 'Board.controller - create')
+
     return this.boardsService.create(file, userId, createBoardDto)
   }
 
@@ -51,10 +54,13 @@ export class BoardsController {
   }
   
   //어드민은 그냥 다 삭제할 수 있도록 해둘까?
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':board_id')
-  remove(@Request() req, @Param() board_id: number) {
+  remove(@Request() req, @Param() params) {
     const userId = req.user.id
-    logger.traceLogger(`입력 정보 = req.user: ${JSON.stringify(req.user)} & board_id: ${board_id}`, 'Board.controller - remove')
+    const board_id = params.board_id
+    logger.traceLogger(`입력 정보 = req.user: ${JSON.stringify(req.user)} & board_id: ${JSON.stringify(board_id)}`, 'Board.controller - remove')
+    console.log('국밥1', board_id)
     return this.boardsService.remove(userId, board_id)
   }
 }
