0b3fe84f48ca4a0686bfefc89642e2cb63c30c9f
cocojerry1 <ymchoi526@gmail.com>
2024-04-22 21:50:52 +0900

    쿼리빌더

--- src/orders/carts.service.ts
+++ src/orders/carts.service.ts
@@ -29,170 +29,168 @@ export class CartService {
   ) { }
 
   // 장바구니 추가
-  async addToCart(
-    userId: number,
-    goodsId: number,
-    createCartDto: CreateCartDto,
-  ) {
-
-    await validation(CreateCartDto, createCartDto)
-
-    if (_.isNil(userId) || userId == 0 || _.isNil(goodsId) || goodsId == 0) {
-      const error = new BadRequestException('잘못된 요청입니다!')
-      logger.errorLogger(error, `userId = ${userId}, goodsId = ${goodsId}, createCartDto = ${JSON.stringify(createCartDto)}`)
-      throw error
+  async addToCart(userId: number, goodsId: number, createCartDto: CreateCartDto) {
+    await validation(CreateCartDto, createCartDto);
+  
+    if (!userId || userId === 0 || !goodsId || goodsId === 0) {
+      const error = new BadRequestException('잘못된 요청입니다!');
+      logger.errorLogger(error, `userId = ${userId}, goodsId = ${goodsId}, createCartDto = ${JSON.stringify(createCartDto)}`);
+      throw error;
     }
-
+  
     const { ctCount } = createCartDto;
-
-    const goods = await this.goodsRepository.findOne({
-      relations: ['stock'],
-      where: {
-        id: goodsId,
-      },
-    });
-
-    const stocks = await this.stocksRepository.findOne({
-      where:{
-        count: ctCount,
-    },
-  });
-
+  
+    const goods = await this.goodsRepository.createQueryBuilder("goods")
+      .leftJoinAndSelect("goods.stock", "stock")
+      .where("goods.id = :goodsId", { goodsId })
+      .getOne();
+  
     if (!goods) {
       const error = new BadRequestException('존재하지 않는 상품입니다.');
-      logger.errorLogger(error, `userId = ${userId}, goodsId = ${goodsId}, createCartDto = ${JSON.stringify(createCartDto)}, goods = ${goods}`)
-      throw error
+      logger.errorLogger(error, `userId = ${userId}, goodsId = ${goodsId}, createCartDto = ${JSON.stringify(createCartDto)}, goods = ${goods}`);
+      throw error;
     }
-
-    const counts = stocks.count - ctCount;
-
-    if (counts < 0) {
+  
+    const stocks = await this.stocksRepository.createQueryBuilder("stock")
+      .where("stock.goods_id = :goodsId", { goodsId })
+      .getOne();
+  
+    const newStockCount = stocks.count - ctCount;
+    if (newStockCount < 0) {
       const error = new BadRequestException('재고가 없습니다.');
-      logger.errorLogger(error, `userId = ${userId}, goodsId = ${goodsId}, createCartDto = ${JSON.stringify(createCartDto)}`)
-      throw error
+      logger.errorLogger(error, `userId = ${userId}, goodsId = ${goodsId}, createCartDto = ${JSON.stringify(createCartDto)}`);
+      throw error;
     }
-
-
-    const existingCartItem = await this.cartsRepository.findOne({
-      where: {
-        user_id: userId,
-        goods_id: goodsId
-      },
-    });
-
+  
+    const existingCartItem = await this.cartsRepository.createQueryBuilder("cart")
+      .where("cart.user_id = :userId AND cart.goods_id = :goodsId", { userId, goodsId })
+      .getOne();
+  
     if (existingCartItem) {
-      // 이미 장바구니에 있는 상품인 경우 수량만 업데이트
       const updateCount = existingCartItem.ct_count + ctCount;
-      return await this.cartsRepository.update({ id: existingCartItem.id }, { ct_count: updateCount })
+      await this.cartsRepository.createQueryBuilder()
+        .update(Carts)
+        .set({ ct_count: updateCount })
+        .where("id = :id", { id: existingCartItem.id })
+        .execute();
+      return await this.cartsRepository.findOne({ where: { id: existingCartItem.id } });
     }
-    // 장바구니에 없는 상품인 경우 새로운 장바구니 아이템 생성
+  
     const newCartItem = this.cartsRepository.create({
       user_id: userId,
       goods_id: goodsId,
       ct_count: ctCount,
       ct_price: goods.g_price,
     });
+  
     return await this.cartsRepository.save(newCartItem);
-
   }
-
+  
 
   // 장바구니 특정 상품 삭제(cartId를 goodsId로 바꾸는게 나을 거 같은데<-완료)
   async removeFromCart(userId: number, goodsId: number) {
-    if (_.isNil(userId) || userId == 0 || _.isNil(goodsId) || goodsId == 0) {
-      const error = new BadRequestException('잘못된 요청입니다!')
-      logger.errorLogger(error, `userId = ${userId}, goodsId = ${goodsId}`)
-      throw error
+    if (!userId || userId === 0 || !goodsId || goodsId === 0) {
+      const error = new BadRequestException('잘못된 요청입니다!');
+      logger.errorLogger(error, `userId = ${userId}, goodsId = ${goodsId}`);
+      throw error;
     }
-
-    const cartInfo = await this.cartsRepository.findOne({
-      where: {
-        goods_id: goodsId,
-        id: userId
-      }
-    })
-
+  
+    const cartInfo = await this.cartsRepository.createQueryBuilder("cart")
+      .where("cart.goods_id = :goodsId AND cart.user_id = :userId", { goodsId, userId })
+      .getOne();
+  
     if (!cartInfo) {
       const error = new NotFoundException('장바구니에 해당 상품이 존재하지 않습니다.');
-      logger.errorLogger(error, `userId = ${userId}, goodsId = ${goodsId}, cartInfo = ${JSON.stringify(cartInfo)}`)
-      throw error
+      logger.errorLogger(error, `userId = ${userId}, goodsId = ${goodsId}, cartInfo = ${cartInfo}`);
+      throw error;
     }
-
-    await this.cartsRepository.delete(
-      {
-        goods_id: goodsId,
-        id: userId
-      });
-
+  
+    await this.cartsRepository.createQueryBuilder()
+      .delete()
+      .from(Carts)
+      .where("goods_id = :goodsId AND user_id = :userId", { goodsId, userId })
+      .execute();
+  
     return {
       message: '상품이 장바구니에서 삭제되었습니다.',
       data: cartInfo
     }
   }
+  
 
   // 장바구니 특정 상품 수량 변경
   async updateQuantity(userId: number, cartId: number, ctCounts: number): Promise<Carts> {
-
-    if (_.isNil(userId) || userId == 0 || _.isNil(cartId) || cartId == 0 || _.isNil(ctCounts) || ctCounts == 0) {
-      const error = new BadRequestException('잘못된 요청입니다!')
-      logger.errorLogger(error, `userId = ${userId}, cartId = ${cartId}`)
-      throw error
+    if (!userId || userId === 0 || !cartId || cartId === 0 || !ctCounts || ctCounts <= 0) {
+      const error = new BadRequestException('잘못된 요청입니다!');
+      logger.errorLogger(error, `userId = ${userId}, cartId = ${cartId}, ctCounts = ${ctCounts}`);
+      throw error;
     }
-
-    if (_.isNil(ctCounts) || ctCounts == 0) {
-      const error = new BadRequestException('변경할 수량을 입력해 주세요.')
-      logger.errorLogger(error, `userId = ${userId}, cartId = ${cartId}, ctCounts = ${ctCounts}`)
-      throw error
-    }
-
-    const cartItem = await this.cartsRepository.findOne({
-      where: { id: cartId },
-    });
-
+  
+    const cartItem = await this.cartsRepository.createQueryBuilder("cart")
+      .where("cart.id = :cartId AND cart.user_id = :userId", { cartId, userId })
+      .getOne();
+  
     if (!cartItem) {
       const error = new NotFoundException('장바구니에 상품을 찾을 수 없습니다.');
-      logger.errorLogger(error, `userId = ${userId}, cartId = ${cartId}, cartItem = ${JSON.stringify(cartItem)}`)
-      throw error
+      logger.errorLogger(error, `userId = ${userId}, cartId = ${cartId}`);
+      throw error;
     }
-
-    cartItem.ct_count = ctCounts;
-    const goods = await this.goodsRepository.findOne({
-      where: { id: cartItem.goods_id },
-    });
+  
+    const goods = await this.goodsRepository.createQueryBuilder("goods")
+      .where("goods.id = :goodsId", { goodsId: cartItem.goods_id })
+      .getOne();
+  
     if (!goods) {
       const error = new NotFoundException('해당 상품을 찾을 수 없습니다.');
-      logger.errorLogger(error, `userId = ${userId}, cartId = ${cartId}, cartItem = ${JSON.stringify(cartItem)}, goods = ${goods}`)
-      throw error
+      logger.errorLogger(error, `userId = ${userId}, cartId = ${cartId}, goodsId = ${cartItem.goods_id}`);
+      throw error;
     }
-    cartItem.ct_price = ctCounts * goods.g_price; // 상품 가격과 수량을 곱하여 총 가격 업데이트
-
-    return this.cartsRepository.save(cartItem);
+  
+    await this.cartsRepository.createQueryBuilder()
+      .update(Carts)
+      .set({
+        ct_count: ctCounts,
+        ct_price: ctCounts * goods.g_price
+      })
+      .where("id = :cartId AND user_id = :userId", { cartId, userId })
+      .execute();
+  
+    return this.cartsRepository.createQueryBuilder("cart")
+      .where("cart.id = :cartId", { cartId })
+      .getOne();
   }
+  
 
   //소비자 장바구니의 모든 상품 조회
   async getCartItems(userId: number): Promise<Carts[]> {
-
-    if (_.isNil(userId) || userId == 0) {
-      const error = new BadRequestException('잘못된 요청입니다!')
-      logger.errorLogger(error, `userId = ${userId}`)
-      throw error
+    if (_.isNil(userId) || userId === 0) {
+      const error = new BadRequestException('잘못된 요청입니다!');
+      logger.errorLogger(error, `userId = ${userId}`);
+      throw error;
     }
-
-    const carts = await this.cartsRepository.find({
-      where: { user_id: userId, },
-    });
-
-    return carts
+  
+    const carts = await this.cartsRepository.createQueryBuilder("cart")
+      .where("cart.user_id = :userId", { userId })
+      .getMany();
+  
+    return carts;
   }
+  
 
 
   //장바구니 비우기
   async clearCart(userId: number): Promise<void> {
-    if (_.isNil(userId) || userId == 0) {
-      const error = new BadRequestException('잘못된 요청입니다!')
-      logger.errorLogger(error, `userId = ${userId}`)
-      throw error
+    if (_.isNil(userId) || userId === 0) {
+      const error = new BadRequestException('잘못된 요청입니다!');
+      logger.errorLogger(error, `userId = ${userId}`);
+      throw error;
     }
-    await this.cartsRepository.delete({ user_id: userId });
+
+    await this.cartsRepository.createQueryBuilder()
+      .delete()
+      .from(Carts)
+      .where("user_id = :userId", { userId })
+      .execute();
   }
 }
+  
--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -8,10 +8,11 @@ import { Point } from '../point/entities/point.entity';
 import { Orders } from '../orders/entities/orders.entity';
 import { OrdersDetails } from '../orders/entities/ordersdetails.entity';
 import { Stocks } from '../goods/entities/stocks.entity';
+import { RedisService } from 'src/redis/redis.service';
 @Module({
     imports: [TypeOrmModule.forFeature([Payments, Users, Point, Orders, OrdersDetails, Stocks])],
-    providers: [PaymentsService],
+    providers: [PaymentsService,RedisService],
     controllers: [PaymentsController],
-    exports: [PaymentsService],
+    exports: [PaymentsService,RedisService],
 })
 export class PaymentsModule { }
