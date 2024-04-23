164d72098848a6ce335f158ab78207f1c71590ce
JY2345 <ilokids@gmail.com>
2024-04-23 13:55:28 +0900

    Feat : 상품검색에 OpenSearch 적용

--- data-migration/index.js
+++ data-migration/index.js
@@ -3,7 +3,6 @@ import mysql from 'mysql2/promise';
 import { Client } from '@opensearch-project/opensearch';
 
 AWS.config.update({ region: 'ap-northeast-2' });
-
 const ssm = new AWS.SSM();
 
 export const handler = async (event) => {
@@ -25,32 +24,40 @@ export const handler = async (event) => {
   });
 
   try {
-    const query = `SELECT * FROM goods WHERE updated_at > ?`;
+    const query = `
+      SELECT g.*, IF(SUM(s.count) > 0, '재고 있음', '재고 없음') AS stock_status
+      FROM goods g
+      LEFT JOIN stocks s ON g.id = s.goods_id
+      WHERE g.updated_at > ?
+      GROUP BY g.id
+    `;
     const [rows] = await sqlConnection.execute(query, [lastMigrationTime]);
-    console.log('Processing records since: ' + lastMigrationTime);
-  
+
+    console.log('마지막 이전 기록 : ' + lastMigrationTime);
     for (const row of rows) {
       const document = {
         id: row.id,
         name: row.g_name,
         price: row.g_price,
         description: row.g_desc,
+        category : row.cate_id,
         image: row.g_img,
         option: row.g_option,
         discountRate: row.discount_rate,
         costPrice: row.cost_price || 0,
         createdAt: row.created_at,
         updatedAt: row.updated_at,
+        stockStatus: row.stock_status // 재고 상태 추가
       };
-  
+
       await esClient.index({
         index: 'goods_index',
         id: row.id.toString(),
         body: document,
       });
     }
-  
-    console.log(`${rows.length} records indexed.`);
+
+    console.log(`${rows.length}개의 데이터가 삽입됨.`);
     await updateLastMigrationTime(new Date().toISOString());
   } catch (error) {
     console.error('Migration error:', error);
