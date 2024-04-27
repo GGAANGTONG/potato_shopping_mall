6265c0eb6b53d2d85d6d74b836db92473b596613
GGANGTONG <qkrds0914@gmail.com>
2024-04-28 03:04:27 +0900

    토스 페이먼츠 구축 중

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,23 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import MainHome from '../components/MainHome.vue';
 import ShoppingCart from '../components/ShoppingCart.vue';
+import PayCash from '../components/PayCash.vue';
+import PayCashValidation from '../components/PayCashValidation.vue';
 
 const routes = [
   {
     path: '/',
     name: 'MainHome',
-    component: MainHome
+    component: MainHome,
   },
   {
     path: '/cart',
     name: 'ShoppingCart',
-    component: ShoppingCart
-  }
+    component: ShoppingCart,
+  },
+  {
+    path: '/payments/payCash',
+    name: 'payCash',
+    component: PayCash,
+  },
+  {
+    path: '/payments/payCashValidation',
+    name: 'PayCashValidation',
+    component: PayCashValidation,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
 
 export default router;
