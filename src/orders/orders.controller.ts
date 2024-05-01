e5a645cc0c4ead051ee4f9df5e98a51cab54e6c0
JY2345 <ilokids@gmail.com>
2024-05-01 15:59:41 +0900

    modified:   src/orders/orders.controller.ts

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -17,15 +17,17 @@ import logger from 'src/common/log/logger';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) { }
-
+  constructor(private readonly ordersService: OrdersService) {}
 
   //상품 주문과 결제가 합쳐져 있음. 분리 필요
   @UseGuards(AuthGuard('jwt'))
   @Post()
   purchase(@Request() req, @Body() createOrderDto: CreateOrderDto) {
     const userId = req.user.id;
-    logger.traceLogger('Orders - purchase', `req.user = ${JSON.stringify(req.user)}, createOrderDto = ${JSON.stringify(createOrderDto)} `)
+    logger.traceLogger(
+      'Orders - purchase',
+      `req.user = ${JSON.stringify(req.user)}, createOrderDto = ${JSON.stringify(createOrderDto)} `,
+    );
     return this.ordersService.purchase(userId, createOrderDto);
   }
 
@@ -41,7 +43,10 @@ export class OrdersController {
   @Get('user')
   async findAllOrderByUser(@Req() req) {
     const userId = req.user.id; // 현재 로그인한 사용자의 ID
-    logger.traceLogger('Orders - findAllOrderByUser', `req.user = ${JSON.stringify(req.user)}`)
+    logger.traceLogger(
+      'Orders - findAllOrderByUser',
+      `req.user = ${JSON.stringify(req.user)}`,
+    );
     return this.ordersService.findAllOrderbyUser(userId);
   }
 
@@ -49,8 +54,16 @@ export class OrdersController {
   @UseGuards(AuthGuard('jwt'))
   @Get('admin')
   async findAllOrderByAdmin() {
-    logger.traceLogger('Orders - findAllOrderByUser', `parameter = none`)
+    logger.traceLogger('Orders - findAllOrderByUser', `parameter = none`);
     return this.ordersService.findAllOrderbyAdmin();
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('today-count')
+  async getTodayOrdersCount(@Request() req) {
+    const userId = req.user.id;
+    const count = await this.ordersService.getTodayOrdersCount(userId);
+    return count;
   }
 
   // 주문 정보 단건 조회
@@ -64,7 +77,7 @@ export class OrdersController {
   @UseGuards(AuthGuard('jwt'))
   @Get('details/:id')
   async findOrdersDetails(@Param('id', ParseIntPipe) id: number) {
-    return await this.ordersService.findOrdersDetails(id)
+    return await this.ordersService.findOrdersDetails(id);
   }
 
   // 주문 취소
@@ -72,22 +85,22 @@ export class OrdersController {
   @Post(':orderId/cancel')
   async cancelOrder(@Req() req, @Param('orderId') orderId: number) {
     try {
-      const userId = req.user.id
+      const userId = req.user.id;
       // 주문 취소 로직을 서비스에서 호출하여 실행합니다.
-      logger.traceLogger('Orders - findAllOrderByUser', `orderId = ${orderId}`)
-      const cancelledOrder = await this.ordersService.cancelOrder(userId, orderId);
+      logger.traceLogger('Orders - findAllOrderByUser', `orderId = ${orderId}`);
+      const cancelledOrder = await this.ordersService.cancelOrder(
+        userId,
+        orderId,
+      );
       return { message: '주문이 취소되었습니다.', order: cancelledOrder };
     } catch (error) {
       if (error instanceof NotFoundException) {
         throw error;
       } else {
         const fatalError = new NotFoundException('주문을 취소할 수 없습니다.');
-        logger.fatalLogger(fatalError, `orderId = ${orderId}`)
-        throw fatalError  // 그 외의 오류는 일반적인 오류 메시지를 반환합니다.
+        logger.fatalLogger(fatalError, `orderId = ${orderId}`);
+        throw fatalError; // 그 외의 오류는 일반적인 오류 메시지를 반환합니다.
       }
     }
   }
-
-
-
-};
+}
