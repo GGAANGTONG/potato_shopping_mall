986b640cd11c43918b81710a4201f272ac849b2a
JY2345 <ilokids@gmail.com>
2024-05-02 11:41:49 +0900

    modified:   src/user/user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -75,6 +75,7 @@
 //     return { message: '삭제 되었습니다' };
 //   }
 
+
 //   // 3단계때 리펙토링
 //   //포인트 조회
 //   @UseGuards(AuthGuard('jwt'))
@@ -92,28 +93,12 @@
 //     return await this.userService.purchasePoints(userId, purchaseAmount);
 //   }
 
+
 // }
-import {
-  Controller,
-  Get,
-  Headers,
-  Res,
-  HttpStatus,
-  Query,
-  Req,
-  UseGuards,
-  Patch,
-  Body,
-  Delete,
-  Param,
-  Post,
-} from '@nestjs/common';
+import { Controller, Get, Headers, Res, HttpStatus, Query, Req, UseGuards, Patch, Body, Delete, Param, Post } from '@nestjs/common';
 
 import { KakaoAuthGuard } from 'src/auth/kakao.guard';
-import {
-  SocialUser,
-  SocialUserAfterAuth,
-} from 'src/user/decorator/user.decorator';
+import { SocialUser, SocialUserAfterAuth } from 'src/user/decorator/user.decorator';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from './users.service';
 import { RedisService } from 'src/redis/redis.service';
@@ -121,77 +106,74 @@ import _ from 'lodash';
 import { UpdateDto } from './dto/update.dto';
 import { AuthGuard } from '@nestjs/passport';
 
+
 @Controller('oauth')
 export class UserController {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
-    private readonly redisService: RedisService,
+    private readonly redisService: RedisService
   ) {}
 
-  // 로그인 & 회원가입//
-  @UseGuards(KakaoAuthGuard)
-  @Get('kakao/callback')
-  async kakaoCallbacks(
-    @SocialUser() socialUser: SocialUserAfterAuth,
-    @Res({ passthrough: true }) res,
-  ): Promise<void> {
-    const { email, nickName } = socialUser;
-    console.log(socialUser);
-    const user = await this.userService.findByEmail(email);
-    console.log(user);
+  
+// 로그인 & 회원가입//
+@UseGuards(KakaoAuthGuard)
+@Get('kakao/callback')
+async kakaoCallbacks (
+  @SocialUser() socialUser: SocialUserAfterAuth,
+  @Res({passthrough: true}) res
+): Promise<void> {
+  const {email, nickName} = socialUser
+  console.log(socialUser)
+  const user = await this.userService.findByEmail(email)
+  console.log(user)
 
-    if (_.isNil(user)) {
-      const user = await this.userService.createUser(email, nickName);
+  if(_.isNil(user)) {
+   const user = await this.userService.createUser(email, nickName)
 
-      const payload = { sub: user.id };
+   const payload = {sub: user.id}
 
-      const accessToken = this.jwtService.sign(payload, {
-        secret: process.env.JWT_SECRET_KEY,
-        expiresIn: 1000 * 60 * 60 * 36,
-      });
+   const accessToken = this.jwtService.sign(payload, {
+    secret: process.env.JWT_SECRET_KEY,
+    expiresIn: 1000 * 60 * 60 * 36,
+  });
 
-      const refreshToken = this.jwtService.sign(payload, {
-        secret: process.env.REFRESH_SECRET,
-        expiresIn: 1000 * 60 * 60 * 24 * 7,
-      });
+  const refreshToken = this.jwtService.sign(payload, {
+    secret: process.env.REFRESH_SECRET,
+    expiresIn: 1000 * 60 * 60 * 24 * 7,
+  }); 
 
-      await this.redisService
-        .getClient()
-        .set(`refreshToken for ${user.id}`, refreshToken);
-      console.log('토큰 발급', accessToken);
-
-      res.cookie('accessToken', `Bearer ${accessToken}`,{
-        path: '/',
-        secure: false,  
-        sameSite: 'Lax'
-      });
-      return res.redirect('http://localhost:3000/');
-    }
-
-    const payload = { sub: user.id };
-    const accessToken = this.jwtService.sign(payload, {
-      secret: process.env.JWT_SECRET,
-      expiresIn: 1000 * 60 * 60 * 12,
-    });
-
-    const refreshToken = this.jwtService.sign(payload, {
-      secret: process.env.JWT_SECRET,
-      expiresIn: 1000 * 60 * 60 * 24 * 7,
-    });
-
-    await this.redisService
-      .getClient()
-      .set(`refreshToken for ${user.id}`, refreshToken);
-    res.cookie('accessToken', `Bearer ${accessToken}`,{
-      path: '/',
-      secure: false,  
-      sameSite: 'Lax'
-    });
-    console.log('카카오 로그인', accessToken);
-    console.log('도메인: ' + process.env.CLIENT_HOST);
-    return res.redirect(301, `http://${process.env.CLIENT_HOST}`);
+  await this.redisService.getClient().set(`refreshToken for ${user.id}`, refreshToken)
+  console.log('토큰 발급', accessToken)
+  res.cookie('accessToken', `Bearer ${accessToken}`)
+  return res.redirect('http://potato-shop.shop/')
   }
+
+
+  const payload = {sub:user.id}
+  const accessToken = this.jwtService.sign(payload, {
+    secret: process.env.JWT_SECRET,
+    expiresIn: 1000 * 60 * 60 * 12,
+  });
+
+  const refreshToken = this.jwtService.sign(payload, {
+    secret: process.env.JWT_SECRET,
+    expiresIn: 1000 * 60 * 60 * 24 * 7,
+  });
+
+
+  await this.redisService.getClient().set(`refreshToken for ${user.id}`, refreshToken);
+  res.cookie('accessToken', `Bearer ${accessToken}`, {
+    domain: "potato-shop.shop",
+    path: '/',
+   // maxAge: 1000 * 60 * 10, 
+   // httpOnly: true, 
+    //secure: true
+  })
+  console.log('카카오 로그인', accessToken);
+  console.log('도메인: '+process.env.CLIENT_HOST);
+  return res.redirect(301, `http://${process.env.CLIENT_HOST}`);
+}
 
   //회원정보 수정(으아아아아아아)
   @UseGuards(AuthGuard('jwt'))
@@ -205,10 +187,10 @@ export class UserController {
   @UseGuards(AuthGuard('jwt'))
   @Delete('delete/:id')
   async remove(@Param('id') id: number) {
-    const data = await this.userService.remove(id);
-    return {
+   const data = await this.userService.remove(id);
+    return { 
       message: '삭제 되었습니다',
-      data,
+      data
     };
   }
 
@@ -216,25 +198,27 @@ export class UserController {
   @UseGuards(AuthGuard('jwt'))
   @Get('find-one')
   async findOne(@Req() req) {
-    const id = req.user.id;
-    const data = await this.userService.findOne(id);
-    return {
+    const id =req.user.id
+   const data = await this.userService.findOne(id);
+    return { 
       message: '회원정보입니다.',
-      data,
+      data
     };
   }
 
   //토큰 갱신
   @Post('token/refresh')
-  postTokenRefresh(@Headers('authorization') rawToken: string, @Res() res) {
-    const token = this.userService.extractTokenFromHeader(rawToken, true);
-    const newAccessToken = this.userService.rotateToken(token);
+  postTokenRefresh(@Headers('authorization') rawToken: string, @Res() res){
+  const token = this.userService.extractTokenFromHeader(rawToken, true);
+  const newAccessToken = this.userService.rotateToken(token);
 
-    res.cookie('accessToken', `${newAccessToken}`);
-
-    return {
-      message: '토큰이 재발급 되었습니다.',
-      accessToken: newAccessToken,
-    };
+  res.cookie('accessToken', newAccessToken)
+  return {
+    message: '토큰이 재발급 되었습니다.',
+    accessToken: newAccessToken,
   }
+  }
+
 }
+
+
